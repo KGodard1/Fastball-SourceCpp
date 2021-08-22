7fcdb40dadf9f1741e23c6ee2dee3c4a78a68194
Karl Godard <godardk@umich.edu>
2021-08-21 20:16:48 -0400

    removed object files from repository

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -5,21 +5,21 @@
 
 using namespace Rcpp;
 
-// fastball
-Rcpp::List fastball(Rcpp::List inputDf, Rcpp::NumericVector dim);
-RcppExport SEXP _backbone_fastball(SEXP inputDfSEXP, SEXP dimSEXP) {
+// fastball_cpp
+Rcpp::List fastball_cpp(Rcpp::List inputDf, Rcpp::NumericVector dim);
+RcppExport SEXP _backbone_fastball_cpp(SEXP inputDfSEXP, SEXP dimSEXP) {
 BEGIN_RCPP
     Rcpp::RObject rcpp_result_gen;
     Rcpp::RNGScope rcpp_rngScope_gen;
     Rcpp::traits::input_parameter< Rcpp::List >::type inputDf(inputDfSEXP);
     Rcpp::traits::input_parameter< Rcpp::NumericVector >::type dim(dimSEXP);
-    rcpp_result_gen = Rcpp::wrap(fastball(inputDf, dim));
+    rcpp_result_gen = Rcpp::wrap(fastball_cpp(inputDf, dim));
     return rcpp_result_gen;
 END_RCPP
 }
 
 static const R_CallMethodDef CallEntries[] = {
-    {"_backbone_fastball", (DL_FUNC) &_backbone_fastball, 2},
+    {"_backbone_fastball_cpp", (DL_FUNC) &_backbone_fastball_cpp, 2},
     {NULL, NULL, 0}
 };
 
